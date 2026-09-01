7515257e08ce1f26078847228bac0c54686a74a4
pjh456 <147148383@qq.com>
2026-09-01 14:42:48 +0800

    feat: 添加建造者模式宏

--- src/core/marco.hpp
+++ src/core/marco.hpp
@@ -17,4 +17,32 @@ public:                                          \
     type &get_##name() noexcept { return name; } \
     const type &get_##name() const noexcept { return name; }
 
+#define CLASS_BUILDER_START(name) \
+    class name##Builder           \
+    {                             \
+        friend name;              \
+                                  \
+    private:                      \
+        name data;                \
+                                  \
+    private:                      \
+        name##Builder() {}        \
+                                  \
+    public:                       \
+        name build() { return data; }
+
+#define CLASS_BUILDER_ATTRIBUTE(name, type, attr) \
+public:                                           \
+    name##Builder &attr(type val)                 \
+    {                                             \
+        data.get_##attr().set_cur(val);           \
+        return *this;                             \
+    }
+
+#define CLASS_BUILDER_END(name) \
+    }                           \
+    ;                           \
+    inline name##Builder name::create() { return {}; }
+
+
 #endif  // INCLUDE_TREELANG_CORE_MARCO_HPP
