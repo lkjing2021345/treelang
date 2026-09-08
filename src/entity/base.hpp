c1a616a6556f96a7d93f5b565359564a2e45d9b4
pjh456 <147148383@qq.com>
2026-09-08 09:38:19 +0800

    feat: 实体添加唯一字符串 id 属性

--- src/entity/base.hpp
+++ src/entity/base.hpp
@@ -1,6 +1,9 @@
 #ifndef INCLUDE_TREELANG_ENTITY_BASE_HPP
 #define INCLUDE_TREELANG_ENTITY_BASE_HPP
 
+#include <string>
+#include <utility>
+
 #include "core/marco.hpp"
 #include "entity/status.hpp"
 
@@ -11,12 +14,14 @@ namespace treelang
         class Entity
         {
         private:
+            std::string id;
             DEFINE_ATTRIBUTE(StatusCollection, status)
 
         public:
-            DEFAULT_CONSTRUCTOR(Entity)
-            Entity(const StatusCollection &stus) : status(stus) {}
-            Entity(StatusCollection &&stus) noexcept : status(stus) {}
+            Entity(std::string eid, StatusCollection stus) : id(std::move(eid)), status(std::move(stus)) {}
+            Entity(std::string eid, StatusCollection &&stus) noexcept : id(std::move(eid)), status(std::move(stus)) {}
+
+            const std::string &get_id() const noexcept { return id; }
         };
     }
 }
